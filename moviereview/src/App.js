4be217dc0ae4c6d84ae7bc2135ec24be36752762
“Jay <jxv210016@utdallas.edu>
2023-04-11 03:30:45 -0500

    front end outline

--- moviereview/src/App.js
+++ moviereview/src/App.js
@@ -4,21 +4,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <header className="App-header">
+     <img src={logo} className="App-logo" alt="logo" /> 
+    <p>
+  <span class="highlight">The best tool to give you movie reviews!</span>
+  </p>
+  {/*
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+    */}
+    <label>
+      <textarea name></textarea>
+    </label>
+    <span class="highlight">Result:</span>
+  </header>
+</div>
+
   );
 }
 
