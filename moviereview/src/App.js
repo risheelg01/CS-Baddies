1934fa62cae8974308d13b022ba40375c6620e9d
“Jay <jxv210016@utdallas.edu>
2023-04-12 01:48:17 -0500

    PENIS

--- moviereview/src/App.js
+++ moviereview/src/App.js
@@ -1,33 +1,51 @@
+import { useState } from 'react';
 import logo from './Logo.svg';
 import './App.css';
 
 function App() {
+  const [textValue, setTextValue] = useState('');
+  const [result, setResult] = useState('');
+
+  function handleTextareaChange(event) {
+    setTextValue(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // Here, you can send a POST request to the backend with the textValue
+    // variable as the payload.
+    console.log('Submitting form with text value:', textValue);
+
+    // Replace this with your actual code to get the result from the backend
+    const mockResult = 'This is the result!';
+    setResult(mockResult);
+  }
+
   return (
     <div className="App">
-  <header className="App-header">
-     <img src={logo} className="App-logo" alt="logo" /> 
-    <p>
-  <span class>The best tool to give you movie reviews!</span>
-  </p>
-  {/*
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
-    */}
-    <label>
-      <textarea name></textarea>
-    </label>
-    <span class>Result:</span>
-  </header>
-</div>
-
-
-
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <span class>The best tool to give you movie reviews!</span>
+        </p>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>
+              <textarea name="text" value={textValue} onChange={handleTextareaChange}></textarea>
+            </label>
+            <br></br>
+            <button type="submit">Review</button>
+          </div>
+        </form>
+        {result && (
+          <div>
+            <span>Result:</span>
+            <p>{result}</p>
+          </div>
+        )}
+      </header>
+    </div>
   );
 }
 
